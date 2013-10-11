6f194dd04e1f5c42f2b7ac40fe22dad87149c482
MichaelKatz <Mike.L.Katz@gmail.com>
2013-10-12 01:32:47 +0300

    another attempt with heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 YetAnotherRailsTutorial::Application.routes.draw do
+  root to: 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
